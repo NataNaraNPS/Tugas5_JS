50e8bdfc9f0fa5ffae43eac48d7537420b50e2c2
NataNaraNPS <143619238+NataNaraNPS@users.noreply.github.com>
2023-10-05 15:26:41 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//array 1 dimensi (array scalar)
+let p1 = {'nip':'111', 'nama':'Budi Santoso','jabatan':'Manager','status':'Menikah','agama':'Islam'};
+let p2 = {'nip':'112', 'nama':'Siti Aminah','jabatan':'Asisten Manager','status':'Belum','agama':'Islam'};
+let p3 = {'nip':'113', 'nama':'Alissa','jabatan':'Supervisor','status':'Menikah','agama':'Kristen Protestan'};
+let p4 = {'nip':'114', 'nama':'I Putu Gede','jabatan':'Staff','status':'Belum','agama':'Hindu'};
+let p5 = {'nip':'115', 'nama':'Sri Rezeki','jabatan':'Staff','status':'Menikah','agama':'Budha'};
+
+//array > 1 dimensi / array of object / array associative
+let pegawai = [p1,p2,p3,p4,p5];
